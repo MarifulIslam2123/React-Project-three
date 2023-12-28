e22bdc1c424c258cb9179888a00a6be5c7ae039f
MarifulIslam2123 <remudhaka@gmail.com>
2023-12-28 14:59:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Carousel from "./components/Carousel"
+import Counter from "./components/Counter"
+import Modal from "./components/Modal"
+import Card from "./components/card"
+
+
+function App() {
+
+
+  return (
+    <>
+      
+      <Carousel/>
+      <Card/>
+      <Counter/>
+      <Modal/>
+    </>
+  )
+}
+
+export default App
